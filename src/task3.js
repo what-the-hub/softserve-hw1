c828a4cf94b125634af9741c767a00ffedbaff58
what-the-hub <iamthenight.yeah@gmail.com>
2021-07-27 15:52:57 +0300

    last comm

--- src/task3.js
+++ src/task3.js
@@ -1,41 +1,36 @@
 enterData();
 
 function enterData() {
-    alert("You need to enter 6 numbers. from 000000 to 999999. Please note that these numbers must be natural numbers.")
+    alert("You need to enter 9 numbers of your ticket. from 000000 to 999999. Please note that these numbers must be natural numbers.")
     let input = prompt("Enter input: ");
     input = checkValue(input);
-
-    
-    // count();
 }
 
 function checkValue(value) {
-    
-    // let tempValue = parseInt(value, 10);
+    let numberArray = [];
     if (value.length === 6) {
-        for(let i = 0; i < 6;  i++){
+        for (let i = 0; i < 6; i++) {
             let newElem = parseInt(value[i]);
-            if (isNaN(newElem)){
+            if (isNaN(newElem)) {
                 alert("Something wrong. Check the values.");
                 enterData();
-            }
-            else{
-                alert ("zbs")
+            } else {
+                numberArray.push(newElem);
             }
         }
-
-    } 
-    else {
+    } else {
         alert("Something wrong. Check the values.");
         enterData();
     }
+    checkNumbers(numberArray);
+}
 
-
-
-    // if (value.length === 6 && value[item] !== NaN && value[item] >= 0, value[item] <=9 && value[item] !== NaN) {
-    //     alert ("goood")
-    // } else {
-    //     alert("Something wrong. Check the number.");
-    //     enterData();
-    // }
+function checkNumbers(array) {
+    let firstPart = array.splice(0, 3);
+    let secondPart = array;
+    if (firstPart[0] + firstPart[1] + firstPart[2] === secondPart[0] + secondPart[1] + secondPart[2]) {
+        alert("you have happy ticket")
+    } else {
+        alert("you dont have happy ticket")
+    }
 }
