48990ba691db19909d340b739d9dce76400a117b
what-the-hub <iamthenight.yeah@gmail.com>
2021-08-11 15:42:47 +0300

    hw6 re

--- hw6/task1.js
+++ hw6/task1.js
@@ -3,9 +3,6 @@ let arr = ["Яблоко", "Банан" , "Ананас"];
 let a = ["Яблоко", "Банан" , "Ананас"].map(el=>el[0]);
 let b = ["ЯБЛОКО", "Банан" , "Ананас"].filter(el=>el[0].toLocaleLowerCase() == "a");
 let c = arr.forEach((el,i,arr) => arr[i] = `${i + 1}: ${el};`);
-console.log(a);
-console.log(b);
-console.log(c);
 
 console.log(arr);
 const arr2 = ["Яблоко", "Банан" , "Ананас"];
@@ -14,22 +11,25 @@ let reduceMap = arr2.reduce(function (newArr, arr2){
     return newArr;
 }, []
 );
-let reduceFilter = arr2.reduce(function (newArr, arr2){
-    if (arr2[0]){
-        arr2[0].toLocaleLowerCase();
+let reduceFilter = arr2.reduce(function (total, el, i){
+    if (i === 0){
+        el[0].toLocaleLowerCase() == "a"
     }
-    return newArr;
-}, []
+    return total;
+
+
+    }
+, []
 );
 
 // .reduce((acc, currentValue, index, arr )
 
-let reduceForEach = arr2.reduce((acc, currentValue, index, arr ) => {
-    currentValue = `${index+1}: ${currentValue}`;
-    return acc;
-    `${index}: ${acc}`
+let reduceForEach = arr2.reduce((newArr, arr2, i) => {
+    newArr.push(`${i + 1}: ${arr2};`);
+    return newArr;
+    } , []
 
-} )
+ )
 console.log(reduceMap, "reduceMap");
 console.log(reduceFilter, "reduceFilter");
 console.log(reduceForEach, "reduceForEach");
